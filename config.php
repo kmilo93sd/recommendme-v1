05a96a4de8e8e1fa85b1348aa8dc0c003456e887
Camilo Alaniz <kmilo93s@gmail.com>
2018-06-12 11:29:11 -0400

    .

--- config.php
+++ config.php
@@ -7,15 +7,15 @@
 const DB_CONFIG = array(
 	"motor" 	=> "mysql",
 	'host' 		=> 'localhost', 
-	'database' 	=> 'recommendme',
-	'username' 	=> 'root',
-	'password' 	=> ''
+	'database' 	=> 'yourdatabasename',
+	'username' 	=> 'user',
+	'password' 	=> 'password'
 );
 
 /*
 	Spotify Credentials
 */ 
 const SPOTIFY_CREDENTIALS = array(
-	'clientId' 	=> '3c8450a73aa34b399c149d047eca4398', 
-	'secretKey' => '201b8f0b70c2465792b96e1767b8f9eb'
+	'clientId' 	=> 'yourClientId', 
+	'secretKey' => 'yourSecretKey'
 );
